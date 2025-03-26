14a52ac5ec1a22c565cdeb9a5fa5d2bb23069cb4
VictorRuizMagallon <vruizinformatica@gmail.com>
2025-03-26 13:47:10 +0100

    .

--- Proyectos/Ajedrez.py
+++ Proyectos/Ajedrez.py
@@ -1,1 +1,18 @@
-# Mas adelante aqui habra un programa que simule el ajedrez =)
+class Ajedrez:
+    tablero = [
+        ["T", "C", "A", "D", "R", "A", "C", "T"],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        ["t", "c", "a", "d", "r", "a", "c", "t"]
+    ]
+
+    # Función para imprimir el tablero
+    def imprimir_tablero(tablero):
+        for fila in tablero:
+            print(" ".join(fila))
+
+    imprimir_tablero(tablero)
