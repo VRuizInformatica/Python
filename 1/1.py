4653420611ee2e3f911e8fc533c1d7da97dd92c5
VictorRuizMagallon <133434561+VRuizInformatica@users.noreply.github.com>
2025-05-12 10:20:40 +0200

    Update 1.py

--- 1/1.py
+++ 1/1.py
@@ -1,11 +1,4 @@
-DEFINE SCHEMA = DELASSMVP
-DEFINE OUTDIR = C:/SQLSpool         -- carpeta SIN espacios (créala si no existe)
-
-SPOOL &OUTDIR/client_inventory_&SCHEMA.lst
-
-SELECT owner, table_name, column_name
-FROM   all_tab_columns
-WHERE  owner = '&SCHEMA'
-  AND  column_name LIKE '%CLIENT%';
-
+SET TERMOUT OFF
+SPOOL C:\SPOOLTEST\prueba.lst
+SELECT 'OK' AS prueba FROM dual;
 SPOOL OFF
